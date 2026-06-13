0cccdf5a79fca7a1aeec6ad5860aa5b0fff00f41
amirhossein-rajabi <amhrajabi09011081831@gmail.com>
2026-06-13 00:50:52 -0700

    Fix and improve unit tests for better coverage

--- tests/test_github_activity.py
+++ tests/test_github_activity.py
@@ -4,7 +4,7 @@ from github_activity import fetch_user_activity
 class TestGitHubActivity(unittest.TestCase):
     def test_invalid_user(self):
         events = fetch_user_activity("this_user_does_not_exist_123456789")
-        self.assertEqual(events, [])  # باید لیست خالی برگرده
+        self.assertEqual(events, [])  
 
 if __name__ == '__main__':
     unittest.main()
